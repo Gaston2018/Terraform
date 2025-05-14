6f23a6e1c37d62fca2fea0d7d7450ecd1f0aec11
Gaston2018 <38983253+Gaston2018@users.noreply.github.com>
2025-05-14 00:57:50 -0300

    Update main.tf

--- AWS EC2 t2.micro/main.tf
+++ AWS EC2 t2.micro/main.tf
@@ -1,11 +1,4 @@
 terraform {
-  cloud {
-    organization = "terraform-gaston"
-    workspaces {
-      name = "terraform-jorge"
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
